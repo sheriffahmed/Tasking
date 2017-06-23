9e8bc7a34c96d3ddc2efc388dbc2ab7c00d532b8
sheriffahmed <sheriff.v.ahmed@gmail.com>
2017-06-22 21:20:54 -0400

    first

---
+++ www/js/controllers.js
@@ -0,0 +1,50 @@
+angular.module('starter.controllers', ["ionic.native"])
+
+.controller('DashCtrl', function($scope) {
+  $scope.openBrowser = functop(){
+  $cordovaInAppBrowser.create("httpsL//www.google.com");
+}
+
+})
+
+.controller('TasksCtrl', function($scope, Tasks, $ionicModal, $cordovaToast, $cordovaLocalNotifications, $cordovaInAppBrowser) {
+  $scope.tasks = Tasks.all();
+  $scope.remove = function(task) {
+    Tasks.remove(task);
+    $cordovaLocalNotifications.schedule({
+      id: 1,
+      text: 'Single ILocalNotification'
+    })
+  };
+
+
+
+  $ionicModal.fromTemplateUrl("templates/modal.html", {
+    scope: $scope,
+    animation: "slide-in-up"
+  }).then(function(modal){
+    $scope.modal = modal;
+  });
+
+
+
+
+
+  $scope.new = {
+    taskName: "",
+    taskDesc: ""
+  };
+    $scope.newTask = function(){
+        if(Tasks.create($scope.new)){
+          // $cordovaToast.show("Task Created", "short", "bottom");
+          $scope.modal.hide();
+        }
+    }
+
+})
+
+.controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
+  $scope.task = Tasks.get($stateParams.taskId);
+})
+
+.controller('AccountCtrl', function($scope) {});
---
+++ www/js/services.js
@@ -0,0 +1,48 @@
+angular.module('starter.services', [])
+
+.factory('Tasks', function() {
+
+  // Some fake testing data
+  var tasks = [
+    {
+      id: 0,
+      name: 'Take out Trash',
+      description: 'I need to make sure I take out the trash before mom gets home'
+    },
+    {
+      id: 1,
+      name: 'Clean Dishes',
+      description: 'Also I need to clean the dishes before anyone gets home. Don\'t need any one to pile more on',
+    },
+    {
+      id: 2,
+      name: 'Feed Kitten',
+      description: 'Gotta feed mittens in like 5 minutes or else he will get a little cranky',
+    }
+  ];
+
+  return {
+    all: function() {
+      return tasks;
+    },
+    create: function(newTask){
+      tasks.push({
+        id: tasks.length + 1,
+        name: newTask.taskName,
+        description: newTask.taskDesc
+      });
+      return true;
+    },
+    remove: function(task) {
+      tasks.splice(tasks.indexOf(task), 1);
+    },
+    get: function(taskId) {
+      for (var i = 0; i < tasks.length; i++) {
+        if (tasks[i].id === parseInt(taskId)) {
+          return tasks[i];
+        }
+      }
+      return null;
+    }
+  };
+});
